d65e0cb4b9900c29881de844007bf0e89a748a67
mw <dalianmengwei@sohu.com>
2017-12-28 15:47:13 +0800

    代码优化

--- DeathlessLocationPod/BGTask.h
+++ DeathlessLocationPod/BGTask.h
@@ -12,4 +12,13 @@
 @interface BGTask : NSObject
 +(instancetype)shareBGTask;
 -(UIBackgroundTaskIdentifier)beginNewBackgroundTask; //开启后台任务
+
+/**
+ *
+ @param all : yes 关闭所有 ,no 只留下主后台任务
+ all:yes 为了去处多余残留的后台任务，只保留最新的创建的
+ *
+ **/
+-(void)endBackGroundTask:(BOOL)all;
 @end
+
